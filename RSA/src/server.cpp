054357e0ec99069b8e990b5235f8ae09d4c3c96f
sainadth <sainadth1123434@gmail.com>
2023-07-06 23:38:49 +0530

    July6-aftersession

--- RSA/src/server.cpp
+++ RSA/src/server.cpp
@@ -3,6 +3,18 @@
 #include <openssl/err.h>
 
 #include "server.h"
+/*
 int main(){
     return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+server::server()
+{
+    cout << "asdf";
+}
+
+server::~server()
+{
 }
---
+++ key_workspace/size.py
@@ -0,0 +1,6 @@
+import sys
+
+text = "sainadth"
+small = "o"
+print(sys.getsizeof(text))
+print(sys.getsizeof(small))
